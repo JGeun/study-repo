e68b9b2f70bcc55eb019d973c485fb15c05d19f3
jgeun <pjk8926@gmail.com>
2025-03-03 22:55:26 +0900

    feat: Custom inject

--- clone-coding/fastcampus-sns/assistedinjection/src/main/java/com/jgeun/assistedinjection/MyApp.kt
+++ clone-coding/fastcampus-sns/assistedinjection/src/main/java/com/jgeun/assistedinjection/MyApp.kt
@@ -1,13 +1,31 @@
 package com.jgeun.assistedinjection
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.migration.CustomInject
+import dagger.hilt.android.migration.CustomInjection
+import javax.inject.Inject
 
 /**
  *
  * @author   JGeun
  * @created  2025/02/25
  */
+class Foo @Inject constructor()
+
 @HiltAndroidApp
+@CustomInject
 class MyApp  : Application() {
+
+	@Inject
+	lateinit var foo: Foo
+
+	override fun onCreate() {
+		Log.e("MyApp",  "foo1  = ${this::foo.isInitialized}")
+		super.onCreate()
+		Log.e("MyApp",  "foo1  = ${this::foo.isInitialized}")
+		CustomInjection.inject(this)
+		Log.e("MyApp",  "foo1  = ${this::foo.isInitialized}")
+	}
 }
