5d53ec9078fc3ff9a67f7f9c5335abb2cd1e1cf8
jgeun <pjk8926@gmail.com>
2025-03-04 09:14:54 +0900

    chore: FC_HOST 임시값 적용

--- clone-coding/fastcampus-sns/data/src/main/java/com/jgeun/fastcampus/sns/data/di/RetrofitModule.kt
+++ clone-coding/fastcampus-sns/data/src/main/java/com/jgeun/fastcampus/sns/data/di/RetrofitModule.kt
@@ -2,7 +2,6 @@ package com.jgeun.fastcampus.sns.data.di
 
 import android.util.Log
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.jgeun.fastcampus.sns.data.constant.FC_HOST
 import com.jgeun.fastcampus.sns.data.retrofit.BoardService
 import com.jgeun.fastcampus.sns.data.retrofit.FCInterceptor
 import com.jgeun.fastcampus.sns.data.retrofit.FileService
@@ -26,10 +25,9 @@ import retrofit2.Retrofit
 
 /**
  *  *[FC_HOST]** 는 로컬 IP 서버 주소를 가리킵니다.
- *  개인 정보 보호를 위해 **직접 생성 후 기입**해주세요.
+ *  개인 정보 보호를 위해 **직접 확인 후 기입**해주세요.
  **/
-
-// const val FC_HOST = "http://123.456.123.14:8080
+private const val FC_HOST = "http://123.456.123.14:8080/"
 
 @Module
 @InstallIn(SingletonComponent::class)
